0ef278f7c3963c926d96d8f691e674f1cf8677e2
Hyesu <hotspam0529@gmail.com>
2018-01-16 22:19:45 +0900

    몰아서하기

--- libLogger/Logger.cpp
+++ libLogger/Logger.cpp
@@ -16,17 +16,32 @@ void Assert(bool assert, const char* file, int line) //, const char* format, ...
 		char temp[LOG_STR_LENGTH];
 		sprintf_s(temp, "assert fail: file[%s], line[%d]", file, line);
 		outFile << temp << std::endl;
-
-		/*va_list arg;
-		va_start(arg, format);
-		sprintf_s(temp, format, arg);
-		va_end(arg);
-
-		outFile << temp << std::endl;*/
 		outFile.close();
 	}
 }
-
+//
+//void AssertExtraInfo(bool assert, const char* file, int line, const char* format, ...)
+//{
+//	if (assert == false)
+//	{
+//		std::ofstream outFile("projectKT.log.txt", g_openMode);
+//		if (g_openMode == std::ofstream::ios_base::out)
+//			g_openMode |= std::ofstream::ios_base::app;
+//
+//		char temp[LOG_STR_LENGTH];
+//		sprintf_s(temp, "assert fail: file[%s], line[%d], ", file, line);
+//		outFile << temp;
+//
+//		va_list arg;
+//		va_start(arg, format);
+//		sprintf_s(temp, format, arg);
+//		va_end(arg);
+//
+//		outFile << temp << std::endl;
+//		outFile.close();
+//	}
+//}
+//
 void Log(const char* format, ...) 
 {
 	std::ofstream outFile("projectKT.log.txt", g_openMode);
--- libLogger/Logger.h
+++ libLogger/Logger.h
@@ -2,5 +2,5 @@
 
 #define ASSERT(assert, ...) Assert(assert, __FILE__, __LINE__)
 
-void Assert(bool assert, const char* file, int line); //, const char* format, ...);
+void Assert(bool assert, const char* file, int line);
 void Log(const char* format, ...);
