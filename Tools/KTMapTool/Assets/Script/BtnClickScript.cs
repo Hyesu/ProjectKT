851a1e5082dc8e3621d700af1bb08e0309400687
geeko <geekor1021@gmail.com>
2018-01-24 00:56:08 +0900

    map tool first commit

---
+++ Tools/KTMapTool/Assets/Script/BtnClickScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnClickScript : MonoBehaviour 
+{
+	public void LoadBtnClicked() 
+	{
+		GameObject loadEdit = GameObject.Find ("LoadEdit");
+		InputField _loadEdit = loadEdit.GetComponent<InputField> ();
+		Debug.Log ("Load btn clicked: " + _loadEdit.text);
+
+		GameObject jsonParser = GameObject.Find ("JsonParser");
+		JsonParser _jsonParser = jsonParser.GetComponent<JsonParser> ();
+		_jsonParser.LoadFrom (_loadEdit.text);
+	}
+
+	public void SaveBtnClicked()
+	{
+		Debug.Log ("save clicked!");
+	}
+}
